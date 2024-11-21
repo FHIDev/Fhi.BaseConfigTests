57e910a3e0d5e03de805521958e75bc993605796
GeirMG <geir.marius.gjul@fhi.no>
2024-11-21 10:07:39 +0100

    Solved last warning. Set treatwarningsaserrors to true. (#4)
    * Solved last warning. Set treatwarningsaserrors to true.
    
    * Added Serializable-attribute to custom exception class

---
+++ Fhi.BaseConfigTests/Exceptions/MissingConfigurationException.cs
@@ -0,0 +1,10 @@
+﻿namespace Fhi.TestUtilities.Exceptions
+{
+    [Serializable]
+    public class MissingConfigurationException : Exception
+    {
+        public MissingConfigurationException() { }
+        public MissingConfigurationException(string message) : base(message) { }
+        public MissingConfigurationException(string message, Exception inner) : base(message, inner) { }
+    }
+}
